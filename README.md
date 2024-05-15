810b80b2170f2c7bdd395a1022ca6c0b03cbcc5f
Craig Cambias <craig.cc@gmail.com>
2024-05-14 22:14:17 -0400

    Building from a clean directory should work

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Estimate average heart rate from a video of a face.
 ## Build
 
 ## Future Improvements
-* The OpenCV build can be minimized to reduce compilation time.
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,37 @@
 #include <iostream>
 
 #include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 
 #include <pocketfft_hdronly.h>
 
 int main(int argc, char ** argv)
 {
+   const std::string video_file = "CodingTest.mov";
+
+   // open video
+   cv::VideoCapture video(video_file);
+
+   if(!video.isOpened()){
+      std::cout << "Failed to open "  << video_file << std::endl;
+      return 0;
+   }
+
+   cv::Mat frame;
+   while(video.read(frame))
+   {
+      cv::imshow("frame", frame);
+      cv::waitKey(10);
+   }
+
+   // sample forehead
+
+   // aggregate in a reasonable manner
+
+   // pass to fft
+
+   // get reasonable frequency
+
    return 0;
 }
