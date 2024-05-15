58273d96bcff85f0bc2109b8b1d280846e0cf351
Craig Cambias <craig.cc@gmail.com>
2024-05-14 22:31:22 -0400

    Get samples from green channel

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <vector>
 
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
@@ -10,21 +11,35 @@
 int main(int argc, char ** argv)
 {
    const std::string video_file = "CodingTest.mov";
+   cv::Rect roi(400, 150, 110, 80);
 
    // open video
    cv::VideoCapture video(video_file);
 
    if(!video.isOpened()){
-      std::cout << "Failed to open "  << video_file << std::endl;
+      std::cerr << "Failed to open "  << video_file << std::endl;
       return 0;
    }
 
-   cv::Mat frame;
+   double fps = video.get(cv::CAP_PROP_FPS);
+
+   std::vector<double> signal;
+
+   cv::Mat3b frame;
    while(video.read(frame))
    {
-      cv::imshow("frame", frame);
-      cv::waitKey(10);
+      double sum = 0.0;
+      for(int r = roi.y; r < roi.y+roi.width; ++r)
+         for(int c = roi.x; c < roi.x+roi.height; ++c)
+         {
+            sum += frame(r,c)(1);
+         }
+      signal.push_back(sum);
+
+      //cv::imshow("frame", frame);
+      //cv::waitKey(0);
    }
+   std::cout << "obtained " << signal.size() << " samples" << std::endl;
 
    // sample forehead
 
