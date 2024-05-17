0ef016536e2525b4e5d40b880292c3f93aec791e
Craig Cambias <craig.cc@gmail.com>
2024-05-17 00:13:58 -0400

    ignore visual studio folder

--- src/main.cpp
+++ src/main.cpp
@@ -23,27 +23,30 @@ int main(int argc, char ** argv)
 
    double fps = video.get(cv::CAP_PROP_FPS);
 
-   std::vector<double> signal;
+   std::vector<double> samples;
 
+   // sample forehead
    cv::Mat3b frame;
    while(video.read(frame))
    {
-      double sum = 0.0;
+      double sample = 0.0;
       for(int r = roi.y; r < roi.y+roi.width; ++r)
          for(int c = roi.x; c < roi.x+roi.height; ++c)
          {
-            sum += frame(r,c)(1);
+            sample += frame(r,c)(1);
          }
-      signal.push_back(sum);
+      samples.push_back(sample);
 
       //cv::imshow("frame", frame);
       //cv::waitKey(0);
    }
    std::cout << "obtained " << signal.size() << " samples" << std::endl;
 
-   // sample forehead
+   shape_t shape_in{samples.size()};
+   stride_t stride_in{sizeof(float)};
 
-   // aggregate in a reasonable manner
+   std::vector<complex<double>> fourier(samples.size());
+   r2c(shape_t{samples.size()}, stride_t{sizeof(double)}, stride_t{sizeof(complex<double>)}, shape_t{0}, true, samples.data(), fourier.data(), ?1.0?, 1);
 
    // pass to fft
 
